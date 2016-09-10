646a662552f707d3f764f18dfed02c2b0788ceaf
Kai-Chu Chung <cage.chung@gmail.com>
2016-09-10 22:42:31 +0800

    fix link doesnot work, add copy project id feature

--- assets/javascript/popup/popupApp.projectListItem.react.js
+++ assets/javascript/popup/popupApp.projectListItem.react.js
@@ -13,94 +13,71 @@ var sprintf = require('sprintf-js').sprintf;
 
 var ProjectListItem = React.createClass({
 
-    highlight: function (text, search) {
-        if (this.props.search.trim()) {
-            text = text.toString();
-            return text.split(search).join('<span class="ui-match">' + search + '</span>');
-        }
-        return text;
-    },
-
-    render: function () {
-
-        var extra;
-        if (this.props.project.status === 1) {
-            extra = <div className="extra">
-                <div className="ui red label small">pending deletion</div>
-            </div>;
-        } else {
-            extra = null;
-
-        }
-
-        return (
-            <div className="item">
-                <div className="content">
-                    <a className="header" onClick={this._openProject}>
-                        <span dangerouslySetInnerHTML={{__html: this.highlight(this.props.project.name, this.props.search)}}></span>
-                        <i className="external icon inline"></i>
-                    </a>
-
-                    <div className="description">
-                        <p>
-                            <span dangerouslySetInnerHTML={{__html: this.highlight(this.props.project.id, this.props.search)}}></span>
-                        </p>
-                    </div>
-                    <div className="ui list">
-                        <a className="item" onClick={this._openAPPENGINE}>GAE : {this.props.project.appEngineProjectId}
-                            <i className="external icon inline"></i>
-                        </a>
-                        <a className="item" onClick={this._openAPPENGINERunning}>GAE : Running
-                            <i className="external icon inline"></i>
-                        </a>
-                        <a className="item" onClick={this._openBilling}>Billing : {this.props.project.numericProjectId}
-                            <i className="external icon inline"></i>
-                        </a>
-                        <a className="item" onClick={this._openMonitoring}>Monitoring :
-                            <span dangerouslySetInnerHTML={{__html: this.highlight(this.props.project.assignedIdForDisplay, this.props.search)}}></span>
-                            <i className="external icon inline"></i>
-                        </a>
-                    </div>
-                {extra}
-                </div>
-            </div>
-        );
-    },
-
-    _openProject: function () {
-        var url = sprintf(settings.PROJECT, {
-            id: this.props.project.id
-        });
-        bg.open(url);
-    },
-
-    _openAPPENGINE: function () {
-        var url = sprintf(settings.APPENGINE, {
-            appEngineProjectId: this.props.project.appEngineProjectId
-        });
-        bg.open(url);
-    },
-
-    _openAPPENGINERunning: function () {
-        var url = sprintf(settings.RUNNING, {
-            id: this.props.project.id
-        });
-        bg.open(url);
-    },
-
-    _openBilling: function () {
-        var url = sprintf(settings.BILLING, {
-            numericProjectId: this.props.project.numericProjectId
-        });
-        bg.open(url);
-    },
-
-    _openMonitoring: function () {
-        var url = sprintf(settings.MONITORING, {
-            id: this.props.project.id
-        });
-        bg.open(url);
+  highlight: function(text, search) {
+    if (this.props.search.trim()) {
+      text = text.toString();
+      return text.split(search).join('<span class="ui-match">' + search + '</span>');
     }
+    return text;
+  },
+
+  render: function() {
+
+    var extra;
+    if (this.props.project.status === 1) {
+      extra = <div className="extra">
+        <div className="ui red label small">pending deletion</div>
+      </div>;
+    } else {
+      extra = null;
+    }
+
+    return (
+      <div className="item">
+        <div className="content">
+          <a className="header" onClick={this._open.bind(this, settings.PROJECT, {id: this.props.project.id})}>
+            <span className="italic" dangerouslySetInnerHTML={{
+              __html: this.highlight(this.props.project.name.trim() || this.props.project.id, this.props.search)
+            }}></span>
+            <i className="external icon inline"></i>
+          </a>
+          <div className="ui list">
+            <a className="item" onClick={this._copy.bind(this, this.props.project.id)}>
+              <span className="italic" dangerouslySetInnerHTML={{
+                __html: this.highlight(this.props.project.id, this.props.search)
+              }}></span>
+              <i className="copy icon inline"></i>
+            </a>
+            <a className="item" onClick={this._open.bind(this, settings.APPENGINE, {id: this.props.project.id})}>App Engine :
+              <span className="italic">{this.props.project.appEngineProjectId}</span>
+              <i className="external icon inline"></i>
+            </a>
+            <a className="item" onClick={this._open.bind(this, settings.BILLING, {numericProjectId: this.props.project.numericProjectId})}>Billing :
+              <span className="italic">
+                {this.props.project.numericProjectId}</span>
+              <i className="external icon inline"></i>
+            </a>
+            <a className="item" onClick={this._open.bind(this, settings.MONITORING, {id: this.props.project.id})}>Monitoring :
+              <span className="italic" dangerouslySetInnerHTML={{
+                __html: this.highlight(this.props.project.assignedIdForDisplay, this.props.search)
+              }}></span>
+              <i className="external icon inline"></i>
+            </a>
+          </div>
+          {extra}
+        </div>
+      </div>
+    );
+  },
+
+  _open: function(pattern, params) {
+    bg.open(sprintf(pattern, params));
+  },
+
+  _copy: function(text) {
+    bg.copy(text);
+    window.close();
+  }
 
 });
 
